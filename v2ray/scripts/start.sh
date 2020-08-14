a7f4d1add2824432ef7f17f13cfd0c3b36d8ff9f
Whale Choi <cj1369636717@gmail.com>
2020-08-15 07:27:56 +0800

    [v1.0.13] Use dnscrypt-proxy to build local dns.

--- v2ray/scripts/start.sh
+++ v2ray/scripts/start.sh
@@ -6,7 +6,9 @@ start_proxy () {
   ${MODDIR}/v2ray.service start &> /data/v2ray/run/service.log && \
   if [ -f /data/v2ray/appid.list ] || [ -f /data/v2ray/softap.list ] ; then
     ${MODDIR}/v2ray.tproxy enable &>> /data/v2ray/run/service.log && \
-    ${MODDIR}/v2ray-dns.service start &>> /data/v2ray/run/service.log &
+    if [ -f /data/v2ray/dnscrypt-proxy/dnscrypt-proxy.toml ] ; then
+      ${MODDIR}/dnscrypt-proxy.service enable &>> /data/v2ray/run/service.log &
+    fi
   fi
 }
 if [ ! -f /data/v2ray/manual ] ; then
