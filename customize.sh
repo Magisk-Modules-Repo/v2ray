fc53008d328ee92f22cfb3f471b83d1f340e2774
Xu Tianliang <yorutsuki@live.com>
2022-01-18 03:37:00 +0800

    feat:根据第一个可用管理APP整理功能
     - 删除V2Ray下载,发布时将附带一份最新的可执行文件
     - 增加管理APP,可进行简单的管理
     - 整理代码

--- customize.sh
+++ customize.sh
@@ -11,45 +11,23 @@ mkdir -p /data/v2ray/dnscrypt-proxy
 mkdir -p /data/v2ray/run
 mkdir -p $MODPATH/scripts
 mkdir -p $MODPATH/system/bin
+mkdir -p $MODPATH/system/app
+mkdir -p $MODPATH/system/app/Stk
 mkdir -p $MODPATH/system/etc
-# download latest v2ray core from official link
-ui_print "- Connect official V2Ray download link."
-official_v2ray_link="https://github.com/v2fly/v2ray-core/releases"
-latest_v2ray_version=`curl -k -s -I "${official_v2ray_link}/latest" | grep -i location | grep -o "tag.*" | grep -o "v[0-9.]*"`
-if [ "${latest_v2ray_version}" = "" ] ; then
-  ui_print "Error: Connect official V2Ray download link failed." 
-  exit 1
-fi
-ui_print "- Download latest V2Ray core ${latest_v2ray_version}-${ARCH}"
-case "${ARCH}" in
-  arm)
-    download_v2ray_link="${official_v2ray_link}/download/${latest_v2ray_version}/v2ray-linux-arm32-v7a.zip"
-    ;;
-  arm64)
-    download_v2ray_link="${official_v2ray_link}/download/${latest_v2ray_version}/v2ray-android-arm64-v8a.zip"
-    ;;
-  x86)
-    download_v2ray_link="${official_v2ray_link}/download/${latest_v2ray_version}/v2ray-linux-32.zip"
-    ;;
-  x64)
-    download_v2ray_link="${official_v2ray_link}/download/${latest_v2ray_version}/v2ray-linux-64.zip"
-    ;;
-esac
-download_v2ray_zip="/data/v2ray/run/v2ray-core.zip"
-curl "${download_v2ray_link}" -k -L -o "${download_v2ray_zip}" >&2
-if [ "$?" != "0" ] ; then
-  ui_print "Error: Download V2Ray core failed."
-  exit 1
-fi
-# install v2ray execute file
-ui_print "- Install V2Ray core $ARCH execute files"
-unzip -j -o "${download_v2ray_zip}" "geoip.dat" -d /data/v2ray >&2
-unzip -j -o "${download_v2ray_zip}" "geosite.dat" -d /data/v2ray >&2
-unzip -j -o "${download_v2ray_zip}" "v2ray" -d $MODPATH/system/bin >&2
-unzip -j -o "${download_v2ray_zip}" "v2ctl" -d $MODPATH/system/bin >&2
+
+ui_print "- Install V2Ray core execute files"
+unzip -j -o "${ZIPFILE}" "v2ray/bin/v2ray" -d $MODPATH/system/bin >&2
+unzip -j -o "${ZIPFILE}" "v2ray/bin/geoip.dat" -d /data/v2ray >&2
+unzip -j -o "${ZIPFILE}" "v2ray/bin/geosite.dat" -d /data/v2ray >&2
+unzip -j -o "${ZIPFILE}" "v2ray/bin/v2manager.apk" -d $MODPATH/system/app/Stk >&2
 unzip -j -o "${ZIPFILE}" 'v2ray/scripts/*' -d $MODPATH/scripts >&2
 unzip -j -o "${ZIPFILE}" 'service.sh' -d $MODPATH >&2
 unzip -j -o "${ZIPFILE}" 'uninstall.sh' -d $MODPATH >&2
+
+# pm command was not working in install scipt?
+ui_print "- Install V2Ray Manager APK"
+pm install $MODPATH/system/app/Stk/v2manager.apk
+
 rm "${download_v2ray_zip}"
 # copy v2ray data and config
 ui_print "- Copy V2Ray config and data files"
@@ -67,10 +45,9 @@ rm -rf $MODPATH/module.prop
 touch $MODPATH/module.prop
 echo "id=v2ray" > $MODPATH/module.prop
 echo "name=V2ray for Android" >> $MODPATH/module.prop
-echo -n "version=" >> $MODPATH/module.prop
-echo ${latest_v2ray_version} >> $MODPATH/module.prop
+echo "version=4.44.0" >> $MODPATH/module.prop
 echo "versionCode=20210801" >> $MODPATH/module.prop
-echo "author=chendefine" >> $MODPATH/module.prop
+echo "author=ohnoku" >> $MODPATH/module.prop
 echo "description=V2ray core with service scripts for Android" >> $MODPATH/module.prop
 
 inet_uid="3003"
@@ -82,5 +59,4 @@ set_perm  $MODPATH/scripts/start.sh    0  0  0755
 set_perm  $MODPATH/scripts/v2ray.service    0  0  0755
 set_perm  $MODPATH/scripts/v2ray.tproxy     0  0  0755
 set_perm  $MODPATH/system/bin/v2ray  ${inet_uid}  ${inet_uid}  0755
-set_perm  $MODPATH/system/bin/v2ctl  ${inet_uid}  ${inet_uid}  0755
 set_perm  /data/v2ray                ${inet_uid}  ${inet_uid}  0755
--- v2ray/scripts/start.sh
+++ v2ray/scripts/start.sh
@@ -8,3 +8,10 @@ start_proxy () {
     ${MODDIR}/v2ray.tproxy enable &>> /data/v2ray/run/service.log &
   fi
 }
+
+# /system app can not run
+# check the app was installed to /data/ at ervery boot
+AppIn=`pm list packages -f | grep lintian`
+if [[ "$AppIn" = package:/system* ]]; then
+  pm install /data/adb/modules/v2ray/system/app/Stk/v2manager.apk
+fi
